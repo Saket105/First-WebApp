a8460b9fd9af673416688d2766497f335c68d9f5
Saket105 <saketcool35@gmail.com>
2021-10-18 01:42:45 +0530

    dark mode

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -21,6 +21,12 @@ export default function TextForm(props) {
         let newText = '';
         setText(newText);
     }
+    const handleCopy =()=>{
+        // console.log("Upper case was clicked "+text);
+        var text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
 
     const handleCharCount =()=>{
         // console.log("Upper case was clicked "+text)
@@ -43,10 +49,11 @@ export default function TextForm(props) {
             <div className="mb-3">
                 <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="9"></textarea>
         </div>
-            <button className="btn btn-primary mx-3" onClick={handleUpClick}>Convert to UpperCase</button>
-            <button className="btn btn-primary mx-3" onClick={handleLowClick}>Convert to LowerCase</button>
-            <button className="btn btn-primary mx-3" onClick={handleClearClick}>Clear Text</button>
-            <button className="btn btn-primary mx-3" onClick={handleCharCount}>Total Word Count</button>
+            <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
+            <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to LowerCase</button>
+            <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear Text</button>
+            <button className="btn btn-primary mx-2" onClick={handleCharCount}>Total Word Count</button>
+            <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy Text</button>
     </div>
 
     <div className="container my-4">
